7faa2486abd9e80a6cf9a19cf6b1a5a9bfd43ce7
leilafaez <leila.faez@gmail.com>
2023-10-03 09:50:52 +0100

    check role on server for authorized user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { expressjwt: expressJwt } = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
+const config = require("../config/dev")
 
 //authentiction middleware
 //this middleware will check access token in authorization header of a req
@@ -15,3 +16,14 @@ exports.checkJwt = expressJwt({
   issuer: "https://dev-fy2n2pbn1lv4tdoo.us.auth0.com/",
   algorithms: ["RS256"],
 });
+ exports.checkRole =(role) => (req,res,next)=>{
+  const user = req.user;
+  if(user && user[config.AUTH0_NAMESPACE + "/roles"].includes(role)){
+    next();
+  }else{
+    res.status(401).send("You are not authorized to access the resource!");
+
+  }
+
+ }
+//  const midd = (req, res, next) => {};
